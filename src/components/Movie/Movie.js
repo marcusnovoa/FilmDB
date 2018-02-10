3be3e6ef0aae6a69c3c6d3f2c51b3d0ccecddec0
Marcus <marcus@marcuscodes.com>
2018-02-10 17:21:08 -0500

    Installed react-overdrive for animations. Additional styles

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Overdrive from 'react-overdrive';
 import './Movie.css';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
@@ -8,7 +9,9 @@ const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 const Movie = ({ movie }) => (
     <div className="Movie">
         <Link to={`/${movie.id}`}>
-            <img className="card thumbnail" src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+            <Overdrive id={`${movie.id}`}>
+                <img className="card thumbnail" src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+            </Overdrive>
         </Link>
     </div>
 );
--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Overdrive from 'react-overdrive';
 import './MovieDetail.css';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
@@ -27,7 +28,9 @@ class MovieDetail extends Component {
         return (
             <div className="MovieDetail">
                 <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-                <img className="card" src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+                <Overdrive id={`${movie.id}`}>
+                    <img className="card" src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+                </Overdrive>
                 <h1>{movie.title}</h1>
                 <h3>{movie.release_date}</h3>
                 <p>{movie.overview}</p>
