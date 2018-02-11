0be1a00ac7d22a9b1e65d72c85545654424b8f7e
Marcus <marcus@marcuscodes.com>
2018-02-11 18:08:38 -0500

    Added Search capabilities to MoviesList component

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,11 +4,17 @@ import './MoviesList.css';
 import Movie from '../Movie/Movie';
 
 class MoviesList extends Component {
-    state = {
-        movies: [],
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            movies: [],
+        };
+
+        this.keywordSearch = this.keywordSearch.bind(this);
     }
 
-    async componentDidMount() {
+    async onLoadResults() {
         try {
             const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_THEMOVIEDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
             const movies = await res.json();
@@ -18,6 +24,31 @@ class MoviesList extends Component {
             });
         } catch (err) {
             console.log(err);
+        }
+    }
+
+    componentDidMount() {
+        this.onLoadResults();
+    }
+    
+    async keywordSearch() {
+        const keyword = document.getElementById('search').value;
+
+        if(keyword !== '') {
+            try {
+                const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_THEMOVIEDB_API_KEY}&query=${keyword}&page=1`);
+                const movies = await res.json();
+
+                this.setState({
+                    movies: movies.results,
+                });
+
+                // console.log(movies)
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            this.onLoadResults();
         }
     }
 
@@ -32,14 +63,12 @@ class MoviesList extends Component {
                         </div>
                     </div>
                     <div className="row filter">
-                        <div className="col s12 m4 white-text">
-                            <p>Search</p>
-                        </div>
-                        <div className="col s12 m4 white-text">
-                            <p>Genres</p>
-                        </div>
-                        <div className="col s12 m4 white-text">
-                            <p>Keywords</p>
+                        <div className="col s12 white-text">
+                            <div className="input-field">
+                                <i className="material-icons prefix">search</i>
+                                <input id="search" type="text" className="search-input" onChange={this.keywordSearch}></input>
+                                <label htmlFor="search" data-error="wrong" data-success="right">Search for Movies and TV Shows...</label>
+                            </div>
                         </div>
                     </div>
                 </div>
